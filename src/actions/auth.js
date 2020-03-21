d3db65e178f5869502da63dc7f1f63f926466a08
Dawid Malec <dawid.malec@nopio.com>
2020-03-21 14:38:42 +0100

    authorization login with token

---
+++ src/actions/auth.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+//types
+export const AUTHENTICATED = 'authenticated_user';
+export const UNAUTHENTICATED = 'unauthenticated_user';
+export const AUTHENTICATION_ERROR = 'authentication_error';
+
+const URL = 'https://reqres.in';
+
+export function signInAction({ email, password }, history) {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post(`${URL}/api/login`, { email, password });
+      dispatch({ type: AUTHENTICATED });
+      localStorage.setItem('user', res.data.token);
+      history.push('/dashboard');
+    } catch(error) {
+      dispatch({
+        type: AUTHENTICATION_ERROR,
+        payload: 'Invalid email or password'
+      });
+    }
+  };
+};
+
+export function signOutAction() {
+    localStorage.clear();
+    return {
+      type: UNAUTHENTICATED
+    };
+  }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
+import Navbar from './components/universal/Navbar';
+import Homepage from './pages/Homepage';
+import Signin from './components/auth/Signin';
+//import Signup from './components/auth/Signup';
+import reducers from './reducers';
+import requireAuth from './components/hoc/RequireAuth';
+import noRequireAuth from './components/hoc/noRequireAuth';
+import { AUTHENTICATED } from './actions/auth';
+import Dashboard from './pages/Dashboard';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const user = localStorage.getItem('user');
+if(user) {
+  store.dispatch({ type: AUTHENTICATED });
+}
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Homepage} />
+        <Route path="/signin" component={noRequireAuth(Signin)} />
+        {/* <Route path="/signup" component={noRequireAuth(Signup)} /> */}
+        {/* <Route path="/signout" component={requireAuth(Signout)} /> */}
+        <Route path="/dashboard" component={requireAuth(Dashboard)} />
+      </div>
+    </Router>
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
