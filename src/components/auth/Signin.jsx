2fe1965840c765d0dd11caa8de6ee8ba90f06369
Dawid Malec <dawid.malec@nopio.com>
2020-04-13 20:39:59 +0200

    replace redux-form -> react hooks form, because YES

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,115 +1,94 @@
-import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form'
 
-import clsx from "clsx";
-import PermIdentity from "@material-ui/icons/PermIdentity";
-import Lock from "@material-ui/icons/Lock";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Container from "@material-ui/core/Container";
-import { signInAction } from "../../actions/auth";
-import css from "./Signin.module.scss";
-import AuthButton from "../UI/AuthButton/AuthButton";
+import clsx from 'clsx';
+import PermIdentity from '@material-ui/icons/PermIdentity';
+import Lock from '@material-ui/icons/Lock';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import { signInAction } from '../../actions/auth';
+import css from './Signin.module.scss';
+import AuthButton from '../UI/AuthButton/AuthButton';
 
-const renderTextField = ({
-  label,
-  input,
-  meta: { touched, invalid, error },
-  ...custom
-}) => (
-  <TextField
-    label={label}
-    error={touched && invalid}
-    helperText={touched && error}
-    {...input}
-    {...custom}
-  />
-);
-class Signin extends Component {
-  submit = (values) => {
-    const { signIn, history } = this.props;
+const SignInForm = ({
+  signIn, history, errorMessage,
+}) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (values) => {
     signIn(values, history);
   };
 
-  errorMessage() {
-    const { errorMessage } = this.props;
+  const errorMessageEl = () => {
     if (errorMessage) {
       return <div className="info-red">{errorMessage}</div>;
     }
-    return "";
+    return '';
   }
 
-  render() {
-    const { handleSubmit } = this.props;
-
-    return (
-      <div className={css.Form}>
-        <form onSubmit={handleSubmit(this.submit)}>
-          <Container maxWidth="sm">
-            <Grid
-              container
-              className={clsx(css.gridWithLine, css.grid, css.gridTop)}
-            >
-              <PermIdentity
-                className={clsx(css.icon)}
-                color="primary"
-                fontSize="large"
-              />
-              <Field
-                name="email"
-                component={renderTextField}
-                label="Username / Email"
-                InputLabelProps={{
-                  className: clsx(css.TextUppercase, css.inputText),
-                }}
-                className={css.TextField}
-                id="email"
-              />
-            </Grid>
-            <Grid container className={clsx(css.grid, css.gridBottom)}>
-              <Lock
-                className={clsx(css.icon)}
-                color="primary"
-                fontSize="large"
-              />
-              <Field
-                name="password"
-                component={renderTextField}
-                label="Password"
-                InputLabelProps={{
-                  className: clsx(css.TextUppercase, css.inputText),
-                }}
-                className={css.TextField}
-                id="password"
-              />
-            </Grid>
-            <AuthButton text="Log In" type="submit" />
-          </Container>
-        </form>
-        {this.errorMessage()}
-      </div>
-    );
-  }
+  return (
+    <div className={css.Form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container maxWidth="sm">
+          <Grid
+            container
+            className={clsx(css.gridWithLine, css.grid, css.gridTop)}
+          >
+            <PermIdentity
+              className={clsx(css.icon)}
+              color="primary"
+              fontSize="large"
+            />
+            <TextField
+              inputRef={register}
+              label="Username / Email"
+              InputLabelProps={{
+                className: clsx(css.TextUppercase, css.inputText),
+              }}
+              name="email"
+              className={css.TextField}
+              id="email"
+            />
+          </Grid>
+          <Grid container className={clsx(css.grid, css.gridBottom)}>
+            <Lock
+              className={clsx(css.icon)}
+              color="primary"
+              fontSize="large"
+            />
+            <TextField
+              inputRef={register}
+              label="Password"
+              InputLabelProps={{
+                className: clsx(css.TextUppercase, css.inputText),
+              }}
+              className={css.TextField}
+              name="password"
+              id="password"
+            />
+          </Grid>
+          <AuthButton text="Log In" type="submit" />
+        </Container>
+      </form>
+      {errorMessageEl()}
+    </div>
+  );
 }
 
-Signin.propTypes = {
+SignInForm.propTypes = {
   signIn: PropTypes.func.isRequired,
   history: PropTypes.func.isRequired,
   errorMessage: PropTypes.string,
-  handleSubmit: PropTypes.func.isRequired,
 };
-Signin.defaultProps = {
+SignInForm.defaultProps = {
   errorMessage: false,
 };
 
 const mapStateToProps = (state) => ({ errorMessage: state.auth.error });
 
-const reduxFormSignin = reduxForm({
-  form: "signin",
-})(Signin);
-
 export default connect(mapStateToProps, { signIn: signInAction })(
-  reduxFormSignin
+  SignInForm,
 );
